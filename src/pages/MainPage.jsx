c6b4063e7c50d9967875dd2173c31fa8c5ba9253
21shunya <shabanchick@gmail.com>
2022-08-11 15:26:21 +0300

    fix(MainPage): update MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Logo from "../Components/Logo";
+import MainPageNavbar from "../Components/MainPageNavbar";
 import "../styles/MainPage.css"
+import info from "../aboutInfo.json"
 
 function MainPage() {
   return (
     <div className="page-wrapper">
       <div className="content-wrapper">
-        
+        <div className="header">
+          <MainPageNavbar />
+          <Logo />
+        </div>
+        <div className="content">
+          <h2>{info.title}</h2>
+          {info.description.map(paragraph =>
+          <p>{paragraph}</p>
+          )}
+        </div>
       </div>
     </div>
   )
