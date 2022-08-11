62e4b6810cc59564f7a9cd31f7bbb53b5abc2584
21shunya <shabanchick@gmail.com>
2022-08-11 13:27:17 +0300

    fix(app): add styles

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "../styles/MainPage.css"
 
 function MainPage() {
   return (
-    <div>
-      Main Page
+    <div className="page-wrapper">
+      <div className="content-wrapper">
+        
+      </div>
     </div>
   )
 }
